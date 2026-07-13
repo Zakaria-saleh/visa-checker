76593850790da05b7b33dd94f25ff5fa73b5221c
Zakaria-saleh <zalsdy475@gmail.com>
2026-07-13 06:39:08 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,4 @@
-   import multiprocessing
-   
-   # Timeout increased to handle long-running visa checks
-   timeout = 120  # 2 minutes instead of 30 seconds
-   workers = multiprocessing.cpu_count() * 2 + 1
-   worker_class = 'sync'
-   keepalive = 5
+   timeout = 120
+workers = 2
+worker_class = 'sync'
+keepalive = 5
